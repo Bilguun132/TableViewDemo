9a4be2b24cd46b639995bb31141ec2f24d314c56
Bilguun <bilguun132@gmail.com>
2019-02-22 10:17:20 +0800

    Initial Commit

---
+++ TableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Bilguun Batbold on 22/2/19.
+//  Copyright © 2019 ISEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
